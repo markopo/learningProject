d59c950d6cff2e3364d5e5d2a56dba73f0a3d051
markopedu <69790644+markopedu@users.noreply.github.com>
2023-06-27 15:28:56 +0200

    1 st commit

---
+++ src/main/java/com/example/learningproject/controllers/DemoController.java
@@ -0,0 +1,32 @@
+package com.example.learningproject.controllers;
+
+import com.example.learningproject.common.Coach;
+import com.example.learningproject.common.CricketCoach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    private Coach myCoach;
+
+
+    @Autowired
+    public DemoController(@Qualifier("swimCoach") Coach theCoach) {
+        System.out.println("In cstr: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "check";
+    }
+}
