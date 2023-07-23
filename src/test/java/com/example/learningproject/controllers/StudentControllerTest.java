2e2478aeffdf81037f62f113dd7b12f4a55c4095
markopedu <69790644+markopedu@users.noreply.github.com>
2023-07-23 16:49:51 +0200

    Update StudentControllerTest.java

--- src/test/java/com/example/learningproject/controllers/StudentControllerTest.java
+++ src/test/java/com/example/learningproject/controllers/StudentControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.learningproject.controllers;
 
+import com.example.learningproject.dto.StudentDto;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
@@ -32,6 +33,105 @@ public class StudentControllerTest {
                 .contentType(ContentType.JSON)
                 .body("name", equalTo("marko"))
                 .statusCode(200);
+    }
+
+    @Test
+    public void whenFindByNamethenOK() {
+        given()
+                .auth()
+                .basic("mary", "test123")
+                .when()
+                .get(API_ROOT + "/name/marko")
+                .then()
+                .contentType(ContentType.JSON)
+                .body("name", equalTo("marko"))
+                .statusCode(200);
+    }
+
+    @Test
+    public void whenFindByEmail_thenOK() {
+        given()
+                .auth()
+                .basic("mary", "test123")
+                .when()
+                .get(API_ROOT + "/email/marko@gmail.com")
+                .then()
+                .contentType(ContentType.JSON)
+                .body("name", equalTo("marko"))
+                .statusCode(200);
+    }
+
+    @Test
+    public void whenPost_thenOK() {
+        given()
+                .auth()
+                .basic("mary", "test123")
+                .when()
+                .contentType(ContentType.JSON)
+                .body(new StudentDto(0, "banan", "banan@gmail.com"))
+                .post(API_ROOT)
+                .then()
+                .contentType(ContentType.JSON)
+                .body("name", equalTo("banan"))
+                .statusCode(200);
+
+        given()
+            .auth()
+            .basic("mary", "test123")
+            .when()
+            .get(API_ROOT + "/name/banan")
+            .then()
+            .contentType(ContentType.JSON)
+            .body("name", equalTo("banan"))
+            .statusCode(200);
+    }
+
+    @Test
+    public void whenDelete_thenOK() {
+       var responseCreated =  given()
+                                .auth()
+                                .basic("mary", "test123")
+                                .when()
+                                .contentType(ContentType.JSON)
+                                .body(new StudentDto(0, "ola-conny", "ola-conny@gmail.com"))
+                                .post(API_ROOT);
+
+        var olaConny = responseCreated.body().as(StudentDto.class);
+
+        given()
+                .auth()
+                .basic("susan", "test123")
+                .when()
+                .delete(API_ROOT + "/" + olaConny.id())
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void whenPut_thenOK() {
+        var responseCreated =  given()
+                .auth()
+                .basic("mary", "test123")
+                .when()
+                .contentType(ContentType.JSON)
+                .body(new StudentDto(0, "ola-conny", "ola-conny@gmail.com"))
+                .post(API_ROOT);
+
+        var olaConny = responseCreated.body().as(StudentDto.class);
+
+       var responseUpdated =  given()
+                    .auth()
+                    .basic("susan", "test123")
+                    .when()
+                    .contentType(ContentType.JSON)
+                    .body(new StudentDto(olaConny.id(), "morgan", "morgan@gmail.com"))
+                    .put(API_ROOT + "/" + olaConny.id())
+                    .then()
+                    .statusCode(200);
+
+       responseUpdated.body("name", equalTo("morgan"));
+       responseUpdated.body("email", equalTo("morgan@gmail.com"));
+       responseUpdated.body("id", equalTo(olaConny.id()));
     }
 
 
